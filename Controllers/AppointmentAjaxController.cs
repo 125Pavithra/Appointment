5343e889894e0f636e3b7f1031de2b534c7b078e
125Pavithra <125pavithra@gmail.com>
2018-02-28 10:53:02 +0530

    initial commit

---
+++ Controllers/AppointmentAjaxController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InsertAppointment.Controllers
+{
+    public class AppointmentAjaxController : Controller
+    {
+        NewDBAppointmentEntities db = new NewDBAppointmentEntities();
+        // GET: AppointmentAjax
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            return View(db.AppointmentTBLs.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(AppointmentTBL appointment)
+        {
+            var app = db.AppointmentTBLs.SingleOrDefault(c => c.ConsumerName == appointment.ConsumerName);
+            if (app == null)
+            {
+                db.AppointmentTBLs.Add(appointment);
+                db.SaveChanges();
+                //return RedirectToAction("index");
+                return Content("Record Added Successfully");
+            }
+            //return View("index");
+            return Content("Already Existed");
+        }
+    }
+}
